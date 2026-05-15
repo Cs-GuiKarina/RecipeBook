5a8b28da869e0d0a5a8cb8fae7930841e46c80d4
GuiSaiUwU <107002177+GuiSaiUwU@users.noreply.github.com>
2026-05-15 20:07:34 -0300

    feat(frontend): Criado o MenuBar e coisinhas

---
+++ recipebook-frontend/src/app/pages/form/receita-cadastro/receita-cadastro.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-receita-cadastro',
+  imports: [],
+  templateUrl: './receita-cadastro.html',
+  styleUrl: './receita-cadastro.scss',
+})
+export class ReceitaCadastro {}
---
+++ recipebook-frontend/src/app/pages/intro/receita-intro/receita-intro.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-receita-intro',
+  imports: [],
+  templateUrl: './receita-intro.html',
+  styleUrl: './receita-intro.scss',
+})
+export class ReceitaIntro {}
