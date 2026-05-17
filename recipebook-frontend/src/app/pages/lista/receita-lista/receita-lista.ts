527408f0bd2159719f58eacf7178e17bb1152080
Just-Karina <justonekarina@gmail.com>
2026-05-17 18:20:01 -0300

    feat(frontend): Iniciado a produção da listagem de receitas

--- recipebook-frontend/src/app/pages/lista/receita-lista/receita-lista.ts
+++ recipebook-frontend/src/app/pages/lista/receita-lista/receita-lista.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ReceitaService } from '../../../services/receita.service';
+import { Receita } from '../../../models/receita.model';
+import { CardModule } from 'primeng/card';
+import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
+import { PanelModule } from 'primeng/panel';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-receita-lista',
-  imports: [],
+  imports: [CardModule, AsyncPipe, PanelModule, ButtonModule],
   templateUrl: './receita-lista.html',
   styleUrl: './receita-lista.scss',
 })
-export class ReceitaLista {}
+export class ReceitaLista implements OnInit{
+  private receitaService: ReceitaService = inject(ReceitaService);
+
+  receitas: Observable<Receita[]> = this.receitaService.listar();
+
+  ngOnInit() {
+
+  }
+}
