30f1837b83495ce79b64deb11676c2b7504c4ad3
GuiSaiUwU <107002177+GuiSaiUwU@users.noreply.github.com>
2026-05-16 21:43:13 -0300

    feat(frontend): Criado o formulário para cadastro com validações

--- recipebook-frontend/src/app/pages/form/receita-cadastro/receita-cadastro.ts
+++ recipebook-frontend/src/app/pages/form/receita-cadastro/receita-cadastro.ts
@@ -1,9 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AutoFocusModule } from 'primeng/autofocus';
+import { ButtonModule } from 'primeng/button';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputTextModule } from 'primeng/inputtext';
+import { MessageModule } from 'primeng/message';
+import { PanelModule } from 'primeng/panel';
+import { SelectModule } from 'primeng/select';
+import { Categoria, Receita } from '../../../models/receita.model';
+import { ReceitaService } from '../../../services/receita.service';
+import { InputNumberModule } from "primeng/inputnumber";
+import { Textarea, TextareaModule } from "primeng/textarea";
+
+
+interface CategoriaOption {
+  label: string;
+  value: Categoria;
+}
 
 @Component({
   selector: 'app-receita-cadastro',
-  imports: [],
+  imports: [
+    PanelModule,
+    ButtonModule,
+    ReactiveFormsModule,
+    MessageModule,
+    SelectModule,
+    FloatLabelModule,
+    FormsModule,
+    InputTextModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+    FormsModule,
+    InputNumberModule,
+    TextareaModule
+],
   templateUrl: './receita-cadastro.html',
   styleUrl: './receita-cadastro.scss',
 })
-export class ReceitaCadastro {}
+export class ReceitaCadastro {
+  private fb = inject(FormBuilder);
+  private receitaService = inject(ReceitaService);
+
+  form: FormGroup = this.fb.group({
+    nome: ['', [Validators.required, Validators.minLength(3)]],
+    categoria: [Categoria.DOCE, Validators.required],
+    tempoPreparo: [0, [Validators.required, Validators.min(1)]],
+    porcoes: [0, [Validators.required, Validators.min(1)]],
+    ingredientes: [this.fb.array(['']), Validators.required],
+    modoPreparo: ['', [Validators.required, Validators.minLength(10)]]
+  });
+
+  categoriasPossiveis: CategoriaOption[] = Object.values(Categoria).map((categoria) => ({
+    label: categoria,
+    value: categoria
+  }))
+
+
+  salvarReceita() {
+
+  }
+}
