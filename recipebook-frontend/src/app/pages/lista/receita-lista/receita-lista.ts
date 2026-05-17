1359f14a8a6426e8645b5a23ffcb402717b929a5
Just-Karina <justonekarina@gmail.com>
2026-05-17 20:24:13 -0300

    feat(frontend): Finalizado a listagem de receitas e criado a tela de detalhes

--- recipebook-frontend/src/app/pages/lista/receita-lista/receita-lista.ts
+++ recipebook-frontend/src/app/pages/lista/receita-lista/receita-lista.ts
@@ -6,15 +6,22 @@ import { AsyncPipe } from '@angular/common';
 import { Observable } from 'rxjs';
 import { PanelModule } from 'primeng/panel';
 import { ButtonModule } from 'primeng/button';
+import { DrawerModule } from 'primeng/drawer';
 
 @Component({
   selector: 'app-receita-lista',
-  imports: [CardModule, AsyncPipe, PanelModule, ButtonModule],
+  imports: [CardModule, AsyncPipe, PanelModule, ButtonModule, DrawerModule],
   templateUrl: './receita-lista.html',
   styleUrl: './receita-lista.scss',
 })
 export class ReceitaLista implements OnInit{
   private receitaService: ReceitaService = inject(ReceitaService);
+  mostrandoDrawer: boolean = false;
+  idAmostra: number = -1;
+  deixarVisivel(id: number) {
+    this.idAmostra = id;
+    this.mostrandoDrawer = true;
+  }
 
   receitas: Observable<Receita[]> = this.receitaService.listar();
 
