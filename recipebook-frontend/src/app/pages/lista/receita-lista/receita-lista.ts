7fd130c0387afe80bfc8929e56c18e3a6cca2747
Just-Karina <justonekarina@gmail.com>
2026-05-17 21:03:49 -0300

    feat(frontend): mensagem de sucesso ao criar

--- recipebook-frontend/src/app/pages/lista/receita-lista/receita-lista.ts
+++ recipebook-frontend/src/app/pages/lista/receita-lista/receita-lista.ts
@@ -8,25 +8,49 @@ import { PanelModule } from 'primeng/panel';
 import { ButtonModule } from 'primeng/button';
 import { DrawerModule } from 'primeng/drawer';
 import { DividerModule } from 'primeng/divider';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MessageModule } from 'primeng/message';
 
 @Component({
   selector: 'app-receita-lista',
-  imports: [CardModule, AsyncPipe, PanelModule, ButtonModule, DrawerModule, DividerModule, DatePipe],
+  imports: [
+    CardModule,
+    AsyncPipe,
+    PanelModule,
+    ButtonModule,
+    DrawerModule,
+    DividerModule,
+    DatePipe,
+    MessageModule,
+  ],
   templateUrl: './receita-lista.html',
   styleUrl: './receita-lista.scss',
 })
-export class ReceitaLista implements OnInit{
+export class ReceitaLista implements OnInit {
   private receitaService: ReceitaService = inject(ReceitaService);
+  private activatedRoute: ActivatedRoute = inject(ActivatedRoute);
+  private router: Router = inject(Router);
+  sucessoAoCriar: boolean = false;
   mostrandoDrawer: boolean = false;
   idAmostra: number = -1;
   deixarVisivel(id: number) {
     this.idAmostra = id;
     this.mostrandoDrawer = true;
   }
+  removerQuery() {
+    this.router.navigate([], {
+      queryParams: { cadastroSucesso: null },
+      queryParamsHandling: 'merge',
+    });
+  }
+
 
   receitas: Observable<Receita[]> = this.receitaService.listar();
 
   ngOnInit() {
-
+    const sucesso = this.activatedRoute.snapshot.queryParamMap.get('cadastroSucesso');
+    if (sucesso === 'true') {
+      this.sucessoAoCriar = true;
+    }
   }
 }
